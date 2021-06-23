84cf9f6c8b95279c00d0fc53e14591f51ae5d17b
Samuel Anjos <samuelsgabi@outlook.com>
2021-06-23 14:38:17 -0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCore.Interceptors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
